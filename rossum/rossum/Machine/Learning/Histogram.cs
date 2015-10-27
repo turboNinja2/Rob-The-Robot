128aad3df349dd674414a40e27000a1d9c70b198
turboNinja2 <julien.revelle@gmail.com>
2015-10-27 07:55:55 +0100

    Comments

--- rossum/rossum/Machine/Learning/Histogram.cs
+++ rossum/rossum/Machine/Learning/Histogram.cs
@@ -3,7 +3,10 @@ using System.Linq;
 
 namespace rossum.Machine.Learning
 {
-
+    /// <summary>
+    /// Represents the count of a collection of elements of type T.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements to count.</typeparam>
     public class Histogram<T>
     {
         private Dictionary<T, double> _scores = new Dictionary<T, double>(20);
@@ -14,27 +17,47 @@ namespace rossum.Machine.Learning
             get { return _scores; }
         }
 
+        /// <summary>
+        /// Creates an empty histogram.
+        /// </summary>
         public Histogram()
         {
 
         }
 
+        /// <summary>
+        /// Creates an empty histogram whose size is pre-allocated.
+        /// Can optimized memory accesses.
+        /// </summary>
+        /// <param name="preAlloc"></param>
         public Histogram(int preAlloc)
         {
             _scores = new Dictionary<T, double>(preAlloc);
         }
 
+        /// <summary>
+        /// (Deep) copy constructor.
+        /// </summary>
+        /// <param name="model"></param>
         public Histogram(Histogram<T> model)
         {
             _scores = new Dictionary<T, double>(model.Scores);
         }
 
+        /// <summary>
+        /// Builds an histogram from the counts of the elements of an array.
+        /// </summary>
+        /// <param name="rawLabels">The input array.</param>
         public Histogram(T[] rawLabels)
         {
             for (int i = 0; i < rawLabels.Length; i++)
                 UpdateKey(rawLabels[i], 1);
         }
 
+        /// <summary>
+        /// Returns the element with the highest count.
+        /// </summary>
+        /// <returns></returns>
         public T MostLikelyElement()
         {
             if (_scores.Count == 0) return default(T);
@@ -53,6 +76,11 @@ namespace rossum.Machine.Learning
             return mostLikely;
         }
 
+        /// <summary>
+        /// Updates the count. Creates the key if it is not present.
+        /// </summary>
+        /// <param name="key">Key of the element to add.</param>
+        /// <param name="value">Value to add.</param>
         public void UpdateKey(T key, double value)
         {
             if (_scores.ContainsKey(key))
@@ -61,6 +89,11 @@ namespace rossum.Machine.Learning
                 _scores.Add(key, value);
         }
 
+        /// <summary>
+        /// Merges an IList of histogram into one histogram (summing the counts).
+        /// </summary>
+        /// <param name="empiricScores">Input histograms</param>
+        /// <returns>The histogram whose counts are the sum of the counts of the input.</returns>
         public static Histogram<T> Merge(IList<Histogram<T>> empiricScores)
         {
             Histogram<T> result = new Histogram<T>(empiricScores.Select(c => c.Scores.Count).Sum());
@@ -70,6 +103,10 @@ namespace rossum.Machine.Learning
             return result;
         }
 
+        /// <summary>
+        /// Produces a string representation of the histogram : "A:10 B:15 ..."
+        /// </summary>
+        /// <returns></returns>
         public string ToString()
         {
             return string.Join(" ", _scores.Select(c => c.Key.ToString() + ":" + c.Value));
